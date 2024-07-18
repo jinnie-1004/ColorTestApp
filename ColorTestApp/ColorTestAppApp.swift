ed3e84a0b1151cb2b02510ad2200a9403a7ca367
usg <usg@usgui-MacBookAir.local>
2024-07-18 21:45:27 +0900

    Create Xcode Project

---
+++ ColorTestApp/ColorTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorTestAppApp.swift
+//  ColorTestApp
+//
+//  Created by usg on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
